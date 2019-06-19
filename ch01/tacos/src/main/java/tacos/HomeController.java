6843c354714fb672315bf3d5da3bd5bf7e5bc563
changl <changl@flux.com.cn>
2019-06-19 15:11:15 +0800

    1

--- ch01/tacos/src/main/java/tacos/HomeController.java
+++ ch01/tacos/src/main/java/tacos/HomeController.java
@@ -4,7 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller            // <1>
-public class HomeController {
+
+/*In fact, a handful of other annotations (including @Component , @Service , and
+@Repository ) serve a purpose similar to @Controller . You could have just as effec-
+tively annotated HomeController  with any of those other annotations, and it would
+have still worked the same. The choice of @Controller  is, however, more descriptive
+of this component’s role in the application.
+The home()  method is as simple as controlle
+//@Component 
+ 实测 替换后会异常。
+*/public class HomeController {
 
   @GetMapping("/")     // <2>
   public String home() {
